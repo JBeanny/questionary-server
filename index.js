8823cc59dbf7e8148b2fb109478459ba22e788e9
Yim Sotharoth <yimsotharoth999@gmail.com>
2023-01-20 08:32:17 +0700

    scheduling questions and send notification to user who voted the expired question

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const schedule = require("node-schedule");
-const { Server } = require("socket.io");
 const http = require("http");
-const fs = require("fs");
+const notification = require("./lib/middlewares/notification");
 
 require("dotenv").config();
 
@@ -25,60 +23,8 @@ app.use("/api/v1/options", OPTION_API);
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  // console.log(`UID: ${socket.id} is logged in`);
-  // run every 5s
-  schedule.scheduleJob("question-schedule", "*/5 * * * * *", () => {
-    let outOfDeadlineTasks = getOutOfDeadlineTasks();
-    let totalOutDated = [];
-
-    if (outOfDeadlineTasks.length > 0) {
-      outOfDeadlineTasks.forEach((task) => {
-        let totalVote = 0;
-        if (task) {
-          task.answers.forEach((answer) => (totalVote += answer.chosen));
-          totalOutDated.push({
-            id: task.id,
-            question: task.question,
-            votes: totalVote,
-          });
-        }
-      });
-    }
-    socket.emit("notification", {
-      notification: totalOutDated,
-    });
-    schedule.cancelJob("question-schedule");
-  });
-
-  socket.emit("get-uid", {
-    uid: socket.id,
-  });
-});
-
-function getOutOfDeadlineTasks() {
-  const questions = JSON.parse(
-    fs.readFileSync(`${__dirname}/data/questions.json`, "utf-8")
-  );
-
-  const today = new Date();
-
-  const outOfDate = questions.map((question) => {
-    const deadline = new Date(question.end_date);
-
-    if (deadline <= today) {
-      return question;
-    }
-  });
-  return outOfDate || [];
-}
+//to send notificatin when tasks are out of date
+notification(server);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
